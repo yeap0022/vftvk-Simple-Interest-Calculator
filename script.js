cab37f4e567ee98f02fa3f3d45e924716c5414fc
Yeap Yew Ming <37860924+yeap0022@users.noreply.github.com>
2022-05-05 11:25:50 +0800

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function compute()
     }
     var interest = principal * years * rate /100;
     var year = new Date().getFullYear()+parseInt(years);
-    document.getElementById("result").innerHTML="If you deposit <mark>$"+principal+"</mark>,\<br\>at an interest rate of <mark>"+rate+"%</mark>\<br\>You will receive an amount of <mark>$"+interest+"</mark>,\<br\>in the year <mark>"+year+"</mark>\<br\>"
+    document.getElementById("result").innerHTML="If you deposit <mark>$"+principal+"</mark>,\<br\>at an interest rate of <mark>"+rate+"%</mark>.\<br\>You will receive an amount of <mark>$"+interest+"</mark>,\<br\>in the year <mark>"+year+"</mark>\<br\>"
 }
 
 function updateRate() 
