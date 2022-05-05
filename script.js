c7e771c17cfddece4c24d9f38711abeecd301279
Yeap Yew Ming <37860924+yeap0022@users.noreply.github.com>
2022-05-05 09:58:33 +0800

    Update script.js

--- script.js
+++ script.js
@@ -19,6 +19,6 @@ function compute()
 function updateRate() 
 {
     var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText=rateval;
+    document.getElementById("rate_val").innerText=rateval+"%";
 }
         
